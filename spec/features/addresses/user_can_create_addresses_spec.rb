b7600be3aa679501fa57c45edb92b6ce464e9168
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 16:10:53 -0700

    Add addresses to students show

--- spec/features/addresses/user_can_create_addresses_spec.rb
+++ spec/features/addresses/user_can_create_addresses_spec.rb
@@ -14,6 +14,7 @@ describe "user can create addresses" do
       fill_in "address[zipcode]", with: 123456
       click_on "Create Address"
 
+      
       expect(current_path).to eq(student_path(student))
       expect(page).to have_content("It's desolate and there is no roof, but it's home")
       expect(page).to have_content("Off of the beaten path")
