c6f80a1be4e4aff3773160b19d080dfa3f9b46eb
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 15:51:10 -0700

    Add create action

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,6 +4,17 @@ class AddressesController < ApplicationController
     @address = Address.new
   end
 
+  def create
+    student = Student.find(params[:id])
+    @address = student.address.create!(address_params)
+    if @address.save
+      flash[:success] = "You have added a new address"
+      redirect_to sudent_path(@student)
+    else
+      render :new
+    end
+  end 
+
   private
   def address_params
     params.require(:address).permit(:street, :city, :state, :zipecode, :description)
