d18c05226223ce69b55c185498667bc9f8ebd180
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 14:32:08 -0700

    Add new and create methods to controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,25 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have created a new student"
+      redirect_to student_path(@student)
+    else
+      render :new
+      flash[:alert] = "Error. Student not saved... Try again!!!"
+    end
+
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name)
+  end
+
 end
