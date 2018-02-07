e7c554b5e8e274e8a6447270d09936d775a54725
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 16:17:03 -0700

    Add address factory and update students show feature

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,17 @@ FactoryBot.define do
 
   factory :student do
     sequence(:name) { |n| "Krinkle Davenport the #{n} Esq."}
-  end 
+  end
+
+  factory :address do
+    sequence(:street) { |n| "#{n} Street"}
+    city "SnoozeCityYonder"
+    state "CO"
+    zipcode 5
+    description "What have you"
+    student
+  end
+
+
 
 end
