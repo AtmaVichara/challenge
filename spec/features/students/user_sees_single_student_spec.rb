47ce17aaa461d660d4336ada467dd6b643ce0efe
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 13:21:41 -0700

    Create students table and db

--- spec/features/students/user_sees_single_student_spec.rb
+++ spec/features/students/user_sees_single_student_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "user can sees single student" do
   describe "user navigates to student show page" do
     describe "user sees single student" do
-      student = create(:student)
+      student = Student.create!(name: "Nergal Davenport III Esq.")
       visit student_path(student)
 
       expect(page).to have_content(student.name)
