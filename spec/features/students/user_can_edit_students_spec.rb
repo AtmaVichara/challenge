bedda9238944d550ee36eae0950c82ffa4e320f5
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 14:47:38 -0700

    Add feature test for edit student

---
+++ spec/features/students/user_can_edit_students_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "user can edit student" do
+  describe "user links from index page" do
+    it "edits student" do
+      student = create(:student)
+      visit students_path
+      
+      click_on "Edit"
+
+      fill_in "student[name]", with: "Philmeister Omega Spree Jr."
+      click "Submit"
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content("Philmeister Omega Spree Jr.")
+    end
+  end
+end
