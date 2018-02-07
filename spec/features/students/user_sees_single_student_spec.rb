918725cd5821868f66ae11d6fd47a2e78c7c0a83
Joe Jobes <jmrjobes@gmail.com>
2018-02-07 13:29:41 -0700

    Add student model and validation spec

--- spec/features/students/user_sees_single_student_spec.rb
+++ spec/features/students/user_sees_single_student_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "user can sees single student" do
   describe "user navigates to student show page" do
-    describe "user sees single student" do
+    it "user sees single student" do
       student = Student.create!(name: "Nergal Davenport III Esq.")
       visit student_path(student)
 
